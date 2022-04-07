e8cd0c98ff52486c2365ea2892f574ce0cce5759
weilun <gxh0181160809rp@gmail.com>
2022-04-07 15:36:44 +0800

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.1',
+        'version' => '1.1.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'weilun/wlcurl',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'weilun/wlcurl' => array(
+            'pretty_version' => '1.1',
+            'version' => '1.1.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
