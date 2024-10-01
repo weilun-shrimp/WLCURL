8d3f48008e61fa00ef64cf6aee649ba9e2d6adf8
Alan-Hsu <alan.hsu@my-innovate.com>
2024-10-01 19:22:08 +0800

    stash

--- src/Curl.php
+++ src/Curl.php
@@ -9,9 +9,9 @@ class Curl
 
     public string $baseUrl = '';
     public string $path = '';
-    public array $query = [];
+    public array $queries = [];
     public string $method = 'GET';
-    public array $opt = [
+    protected array $_opts = [
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_TIMEOUT => 10,
@@ -22,6 +22,8 @@ class Curl
      * All key value type are constrained to be string.
      */
     protected array $_headers = [];
+
+    public Request $request;
 
 
     public static function __callStatic(string $name, array $arguments)
@@ -70,16 +72,22 @@ class Curl
             case 'headers':
                 return $this->_headers;
                 break;
+            case 'opts':
+                return $this->_opts;
+                break;
             default:
                 return null;
         }
     }
 
-    public function __set(string $name, array $arguments)
+    public function __set(string $name, mixed $arguments)
     {
         switch ($name) {
             case 'headers':
-                $this->_headers = $arguments;
+                $this->headers($arguments);
+                break;
+            case 'opts':
+                $this->opts($arguments);
                 break;
         }
     }
@@ -111,7 +119,7 @@ class Curl
                 break;
             default:
                 throw new \Exception('
-                    Unsupport mode argument be found in ' . self::class . '::' . __FUNCTION__ . '(). 
+                    Unsupport $mode argument be found in ' . self::class . '::' . __FUNCTION__ . '(). 
                     Only support ' . self::class . '::REPLACE_MODE and ' . self::class . '::ADD_MODE.
                 ');
         }
@@ -125,27 +133,36 @@ class Curl
     }
 
     /**
-     * @param array $query - The value that will be replaced the whole $this->query variable or added the key value of the $this->query variable.
+     * Set one query or replace a query if the key is exists.
+     */
+    public function query(string $key, mixed $value)
+    {
+        $this->queries[$key] = $value;
+        return $this;
+    }
+
+    /**
+     * @param array $queries - The value that will be replaced the whole $this->queries variable or added the key value of the $this->queries variable.
      * @param string $mode 
-     *  - Determind the $query argument will be replaced or be added of the $this->query variable.
+     *  - Determind the $queries argument will be replaced or be added of the $this->queries variable.
      *  - Only accept self::REPLACE_MODE or self::ADD_MODE value. Otherwise will throw an exception.
      *  - self::REPLACE_MODE will do the whole replace process.
-     *  - self::ADD_MODE will do the added after or replace key value if the key is the same process.
+     *  - self::ADD_MODE will do the added or replace key value if the key is the same process.
      *  - The default value is the self::REPLACE_MODE.
      * @throws \Exception
      */
-    public function query(array $query, string $mode = self::REPLACE_MODE)
+    public function queries(array $queries, string $mode = self::REPLACE_MODE)
     {
         switch ($mode) {
             case self::REPLACE_MODE:
-                $this->query = $query;
+                $this->queries = $queries;
                 break;
             case self::ADD_MODE:
-                foreach ($query as $k => $v) $this->query[$k] = $v;
+                foreach ($queries as $k => $v) $this->query((string) $k, $v);
                 break;
             default:
                 throw new \Exception(' 
-                    Unsupport mode argument be found in ' . self::class . '::' . __FUNCTION__ . '(). 
+                    Unsupport $mode argument be found in ' . self::class . '::' . __FUNCTION__ . '(). 
                     Only support ' . self::class . '::REPLACE_MODE and ' . self::class . '::ADD_MODE.
                 ');
         }
@@ -169,7 +186,7 @@ class Curl
      *  - Determind the $headers argument will be replaced or be added of the $this->headers variable.
      *  - Only accept self::REPLACE_MODE or self::ADD_MODE value. Otherwise will throw an exception.
      *  - self::REPLACE_MODE will do the whole replace process.
-     *  - self::ADD_MODE will do the added after or replace key value if the key is the same process.
+     *  - self::ADD_MODE will do the added or replace key value if the key is the same process.
      *  - The default value is the self::REPLACE_MODE.
      * @throws \Exception
      */
@@ -181,9 +198,8 @@ class Curl
             if (!is_string($v))
                 throw new \Exception('The value of ' . self::class . '::' . __FUNCTION__ . '() $headers argument must be a string.');
 
-            if ($mode !== self::ADD_MODE)
-                continue;
-            $this->query[$k] = $v;
+            if ($mode !== self::ADD_MODE) continue;
+            $this->header($k, $v);
         }
         switch ($mode) {
             case self::REPLACE_MODE:
@@ -194,10 +210,86 @@ class Curl
                 break;
             default:
                 throw new \Exception(' 
-                    Unsupport mode argument be found in ' . self::class . '::' . __FUNCTION__ . '(). 
+                    Unsupport $mode argument be found in ' . self::class . '::' . __FUNCTION__ . '(). 
                     Only support ' . self::class . '::REPLACE_MODE and ' . self::class . '::ADD_MODE.
                 ');
         }
         return $this;
     }
+
+    /**
+     * Set one opt or replace a opt if the key is exists.
+     */
+    public function opt(int $key, mixed $value)
+    {
+        $this->_opts[$key] = $value;
+        return $this;
+    }
+
+    /**
+     * @param array $headers 
+     *  - The value that will be replaced the whole $this->headers variable or added the key value of the $this->headers variable.
+     *  - All key value are constrained to be string. Otherwise will throw an exception.
+     * @param string $mode 
+     *  - Determind the $headers argument will be replaced or be added of the $this->headers variable.
+     *  - Only accept self::REPLACE_MODE or self::ADD_MODE value. Otherwise will throw an exception.
+     *  - self::REPLACE_MODE will do the whole replace process.
+     *  - self::ADD_MODE will do the added or replace key value if the key is the same process.
+     *  - The default value is the self::REPLACE_MODE.
+     * @throws \Exception
+     */
+    public function opts(array $opts, string $mode = self::REPLACE_MODE)
+    {
+        foreach ($opts as $k => $v) {
+            if (!is_int($k))
+                throw new \Exception('The key of ' . self::class . '::' . __FUNCTION__ . '() $opts argument must be an int.');
+
+            if ($mode !== self::ADD_MODE) continue;
+            $this->opt($k, $v);
+        }
+        switch ($mode) {
+            case self::REPLACE_MODE:
+                $this->_opts = $opts;
+                break;
+            case self::ADD_MODE:
+                // Do nothing. Becuase it is already inserted on the top section.
+                break;
+            default:
+                throw new \Exception(' 
+                    Unsupport $mode argument be found in ' . self::class . '::' . __FUNCTION__ . '(). 
+                    Only support ' . self::class . '::REPLACE_MODE and ' . self::class . '::ADD_MODE.
+                ');
+        }
+        return $this;
+    }
+
+    /**
+     * @param array $body - The value that will be replaced the whole $this->body variable or added the key value of the $this->body variable.
+     * @param string $mode 
+     *  - Determind the $body argument will be replaced or be added of the $this->body variable.
+     *  - Only accept self::REPLACE_MODE or self::ADD_MODE value. Otherwise will throw an exception.
+     *  - self::REPLACE_MODE will do the whole replace process.
+     *  - self::ADD_MODE will do the added or replace key value if the key is the same process.
+     *  - The default value is the self::REPLACE_MODE.
+     * @throws \Exception
+     */
+    public function body(array $body, string $mode = self::REPLACE_MODE)
+    {
+        switch ($mode) {
+            case self::REPLACE_MODE:
+                $this->body = $body;
+                break;
+            case self::ADD_MODE:
+                foreach ($body as $k => $v) $this->body[$k] = $v;
+                break;
+            default:
+                throw new \Exception(' 
+                    Unsupport $mode argument be found in ' . self::class . '::' . __FUNCTION__ . '(). 
+                    Only support ' . self::class . '::REPLACE_MODE and ' . self::class . '::ADD_MODE.
+                ');
+        }
+        return $this;
+    }
+
+    public function exe() {}
 }
--- src/Request.php
+++ src/Request.php
@@ -6,6 +6,40 @@ use CurlHandle;
 
 class Request
 {
-    protected CurlHandle | false $row_curl;
-    public CURL $curl;
+    public Curl $curl;
+
+    public string $baseUrl;
+    public string $path;
+    public array $headers;
+    public array | string $body;
+    public string $query;
+    public array $opts;
+
+    public function __construct(Curl $curl)
+    {
+        $this->curl = $curl;
+        $this->baseUrl();
+    }
+
+    protected function baseUrl()
+    {
+        $this->baseUrl = $this->curl->baseUrl;
+    }
+
+    protected function path()
+    {
+        $this->path = $this->curl->path;
+    }
+
+    protected function headers()
+    {
+        foreach ($this->curl->headers as $key => $value) {
+            $this->headers[] = $key . ': ' . $value;
+        }
+    }
+
+    protected function body()
+    {
+        $this->body = $this->curl->body;
+    }
 }
